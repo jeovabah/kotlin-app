0aabe6d97011a443abf462c013c99a0703dc32ad
Jeova Junior <jrjeova@hotmail.com>
2024-01-22 22:07:30 -0300

    Layout products

--- app/src/main/java/com/marketplace/android/ui/components/ProductCard.kt
+++ app/src/main/java/com/marketplace/android/ui/components/ProductCard.kt
@@ -1,12 +1,9 @@
 package com.marketplace.android.ui.components
 
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -15,7 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.marketplace.android.utils.Product
 
@@ -26,22 +23,29 @@ fun ProductCard(product: Product) {
             .padding(8.dp)
             .width(150.dp)
             .height(180.dp),
-        shape = RoundedCornerShape(10.dp)
+        shape = RoundedCornerShape(10.dp),
     ) {
         Column(
-            modifier = Modifier.padding(8.dp),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
+            verticalArrangement = Arrangement.Top
         ) {
-            // Aqui você adicionaria a imagem do produto. Como placeholder, usaremos um ícone.
+            // Usando um ícone maior para simular o espaço de uma imagem
             Icon(
-                imageVector = Icons.Default.ShoppingCart, // Substitua pelo ícone ou imagem do produto.
-                contentDescription = null,
-                modifier = Modifier.size(100.dp)
+                imageVector = Icons.Default.AccountBox, // Substitua pelo ícone do produto.
+                contentDescription = "Product Image",
+                modifier = Modifier
+                    .height(100.dp)
+                    .fillMaxWidth()
             )
+            Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = product.name,
-                style = MaterialTheme.typography.bodyLarge
+                style = MaterialTheme.typography.bodyLarge,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
             )
             Text(
                 text = "R$ ${product.price}",
